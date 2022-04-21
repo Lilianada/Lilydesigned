6d611526a063b3f7bdf4cbe4ba29d5d4e3b2874e
Lilianada <lilibest30.lb@gmail.com>
2022-04-21 23:49:13 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>designsbylilian</h1>
+        <h1>Designsbylilian</h1>
       </header>
     </div>
   );
