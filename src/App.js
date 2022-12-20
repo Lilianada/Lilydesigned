77ae1248e5beed8d0d2897ab8e800b264bda1128
Lilianada <lilibest30.lb@gmail.com>
2022-12-20 15:06:18 -0500

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 import React from "react";
-=======
-import './App.css';
-import Hero from './components/Hero/Hero';
->>>>>>> fb0e860edde5fbff5b4bd8a6901b379c528a40f5
 
 function App() {
   return (
