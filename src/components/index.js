74729e6df06bd2d2f597a922b6f08a8411c4ef92
Lilianada <lilibest30.lb@gmail.com>
2022-12-20 23:55:36 -0500

    About Me Section

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-export { default as AboutHero } from "./Hero/AboutHero/AboutHero";
+export { default as AboutMe } from "./AboutMe/AboutMe";
 
+export { default as AboutHero } from "./Hero/AboutHero/AboutHero";
 export { default as ErrorBoundary } from "./ErrorBoundary/ErrorBoundary";
 export { default as About } from "../pages/About";
 export { default as Journal } from "../pages/Journal";
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { AboutHero, Header } from "../components";
+import { AboutHero, AboutMe, Header } from "../components";
 
 export default function About() {
   return (
     <main className="mainWrapper">
         <Header />
         <AboutHero />
-        
+        <AboutMe/>
     </main>
   );
 }
