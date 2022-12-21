9f4d9c37dd52012a7f4929bc3f9565da294f9ef6
Lilianada <lilibest30.lb@gmail.com>
2022-12-20 23:31:51 -0500

    Profile description

--- src/components/Hero/AboutHero/AboutHero.jsx
+++ src/components/Hero/AboutHero/AboutHero.jsx
@@ -5,9 +5,21 @@ import "./AboutHero.scss";
 export default function AboutHero() {
   return (
     <section className="aboutHero">
-        <div className="profilePicture">
-            <img src={Pic} alt="Profile" className="img" />
+      <div className="profilePicture">
+        <img src={Pic} alt="Profile" className="img" />
+      </div>
+      <div className="profileDescription">
+        <h3 className="title">
+          Hi - I'm Lilian, a Frontend developer for high impact organizations.
+        </h3>
+        <p className="text">
+          I am passionate about web technologies and enjoys working at the
+          intersection of creativity and user-friendly interfaces. 
+        </p>
+        <div className="buttonWrap">
+            
         </div>
+      </div>
     </section>
   );
 }
