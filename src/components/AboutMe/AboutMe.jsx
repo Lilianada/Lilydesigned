d34cdf8c604cde6bd0e80a4d9724a9d3f306f5cc
Lilianada <lilibest30.lb@gmail.com>
2022-12-21 00:04:17 -0500

    Reviews Section

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./AboutMe.scss";
 
 export default function AboutMe() {
   return (
-    
+    <section className="aboutSection">
+        <h3 className="title">
+            About Me
+        </h3>
+        <div className="aboutDescription">
+            <p className="text">
+             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel nobis numquam pariatur. Hic maxime laborum ratione saepe quaerat cum minus odit iusto similique officia voluptas sit dicta facilis, expedita neque dolore iste odio quas reprehenderit nulla! Aliquid praesentium corporis libero architecto soluta, sed voluptatem nobis ut nesciunt dignissimos similique voluptatum.
+            </p>
+        </div>
+    </section>
   );
 }
