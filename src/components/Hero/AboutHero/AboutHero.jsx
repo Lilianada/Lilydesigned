e573180116d93ee405c63c4c1f820add1e156340
Lilianada <lilibest30.lb@gmail.com>
2022-12-27 09:04:25 -0500

    ..

--- src/components/Hero/AboutHero/AboutHero.jsx
+++ src/components/Hero/AboutHero/AboutHero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Pic from "../../../assets/images/LilyAvatar.jpg";
-import {HiArrowLongDown} from "react-icons/hi2";
+import Resume from "../../../assets/images/LilianResume.pdf";
 import "./AboutHero.scss";
 
 export default function AboutHero() {
@@ -17,8 +17,7 @@ export default function AboutHero() {
           I am mostly interested in bridging the gap between organizations, businesses and their users by creating user friendly UI components and bringing products to life. 
         </p>
         <div className="buttonWrap">
-          <button className="btnPrimary">Curriculum Vitae</button>
-          {/* <button className="btnSecondary">Cover Letter</button> */}
+          <a href={Resume} className="btnPrimary">Curriculum Vitae</a>
         </div>
       </div>
     </section>
