041ab57db1e2cfbc34163aefb1641f2c593cc94b
Lilianada <lilibest30.lb@gmail.com>
2022-12-28 22:07:42 -0500

    ..fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,28 +22,32 @@ export default function Header({ isAuth, signUserOut }) {
     <ErrorBoundary>
       <header className="desktopHeader">
         <nav className="navItems">
-          <Link
-            to="/"
-            className={active === 1 ? "activeLogo" : "logoName"}
+          <div
             onClick={() => isActive(1)}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 32 32"
-              fill="none"
-              style={{ marginRight: ".5rem" }}
+            <Link
+              to="/"
+              className={active === 1 ? "activeLogo" : "logoName"}
             >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M0 16C10.1699 16 16 10.1699 16 0C16 10.1699 21.8301 16 32 16C21.8301 16 16 21.8301 16 32C16 21.8301 10.1699 16 0 16Z"
-                fill="#fff"
-              />
-            </svg>
-            LilyDesigned
-          </Link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 32 32"
+                fill="none"
+                style={{ marginRight: ".5rem" }}
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M0 16C10.1699 16 16 10.1699 16 0C16 10.1699 21.8301 16 32 16C21.8301 16 16 21.8301 16 32C16 21.8301 10.1699 16 0 16Z"
+                  fill="#fff"
+                />
+              </svg>
+              LilyDesigned
+            </Link>
+
+          </div>
 
           <ul className="navList">
             <motion.li
@@ -60,12 +64,12 @@ export default function Header({ isAuth, signUserOut }) {
             </motion.li>
             <motion.li
               className="navItem"
-              onClick={() => isActive(4)}
+              onClick={() => isActive(3)}
               whileHover={{ scale: 0.9 }}
             >
               <Link
                 to="/contact"
-                className={active === 4 ? "activeLink" : "navLink"}
+                className={active === 3 ? "activeLink" : "navLink"}
               >
                 Contact
               </Link>
@@ -73,54 +77,6 @@ export default function Header({ isAuth, signUserOut }) {
           </ul>
         </nav>
       </header>
-
-      <header className="mobileHeader">
-        <Link
-          to="/"
-          className={active === 1 ? "activeLogo" : "logoName"}
-          onClick={() => isActive(1)}
-        >
-          ThingsbyLilian
-        </Link>
-
-        <nav className="navItems">
-          <div className="toggle" onClick={handleToggle}>
-            <FiMenu
-              size={28}
-              fill="white"
-              stroke="white"
-              style={{ cursor: "pointer" }}
-            />
-          </div>
-
-          <ul className={`navList ${toggle ? "show" : ""}`}>
-            <motion.li
-              className="navItem"
-              onClick={() => isActive(2)}
-              whileHover={{ scale: 0.9 }}
-            >
-              <Link
-                to="/portfolio"
-                className={active === 2 ? "activeLink" : "navLink"}
-              >
-                Portfolio
-              </Link>
-            </motion.li>
-            <motion.li
-              className="navItem"
-              onClick={() => isActive(4)}
-              whileHover={{ scale: 0.9 }}
-            >
-              <Link
-                to="/contact"
-                className={active === 4 ? "activeLink" : "navLink"}
-              >
-                Contact
-              </Link>
-            </motion.li>
-          </ul>
-        </nav>
-      </header>
-    </ErrorBoundary>
+      </ErrorBoundary>
   );
 }
