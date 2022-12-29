b2c88053a0a2f334b884734dbcfb37d13deb638c
Lilianada <lilibest30.lb@gmail.com>
2022-12-28 22:16:36 -0500

    ..fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,32 +22,28 @@ export default function Header({ isAuth, signUserOut }) {
     <ErrorBoundary>
       <header className="desktopHeader">
         <nav className="navItems">
-          <div
+          <Link
+            to="./"
+            className={active === 1 ? "activeLogo" : "logoName"}
             onClick={() => isActive(1)}
           >
-            <Link
-              to="./"
-              className={active === 1 ? "activeLogo" : "logoName"}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              style={{ marginRight: ".5rem" }}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 0 32 32"
-                fill="none"
-                style={{ marginRight: ".5rem" }}
-              >
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M0 16C10.1699 16 16 10.1699 16 0C16 10.1699 21.8301 16 32 16C21.8301 16 16 21.8301 16 32C16 21.8301 10.1699 16 0 16Z"
-                  fill="#fff"
-                />
-              </svg>
-              LilyDesigned
-            </Link>
-
-          </div>
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M0 16C10.1699 16 16 10.1699 16 0C16 10.1699 21.8301 16 32 16C21.8301 16 16 21.8301 16 32C16 21.8301 10.1699 16 0 16Z"
+                fill="#fff"
+              />
+            </svg>
+            LilyDesigned
+          </Link>
 
           <ul className="navList">
             <motion.li
