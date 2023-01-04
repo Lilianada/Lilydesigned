70c01281ee4a0980f0744429bf4e34a903c4d358
Lilianada <lilibest30.lb@gmail.com>
2023-01-04 04:48:00 -0500

    SEO for page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { AnimatePresence } from "framer-motion";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
+import {HelmetProvider} from "react-helmet-async"
+import { AnimatePresence } from "framer-motion";
 import { Contact, Home, Portfolio } from "./components";
+import App from "./App";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,15 +12,17 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-        <AnimatePresence>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route exact path="/" index element={<Home />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </AnimatePresence>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+          <AnimatePresence>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route exact path="/" index element={<Home />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </AnimatePresence>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
