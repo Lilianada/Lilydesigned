a074bf8a3a572f94da637da214de2249629126da
Lilian Okeke <lilibest30.lb@gmail.com>
2023-01-14 22:56:00 +0000

    Added languages used for the project

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -24,6 +24,7 @@ export default function Work() {
         <div className="workWrap">
           <div className="project">
             <p className="projectName">FatBossGambling | January 2023 </p>
+            <p className="projectLang">HTML/CSS</p>
             <p className="projectDetails">
               FatBossGambling is an online Casino. State-of-the-art platform that offers a wide selection of exciting casino games, all powered by top-notch software. They've got all your favourites, from classic slots and
               table games to the latest releases. With top-rated graphics and smooth gameplay, you'll feel right in the middle of the action. Plus, withnew games added all the time, you'll always have options.
@@ -46,6 +47,7 @@ export default function Work() {
         <div className="workWrap">
           <div className="project">
             <p className="projectName">BeBlended | July 2022 </p>
+            <p className="projectLang">React/SCSS</p>
             <p className="projectDetails">
               Whether you’re looking for a new hairstylist or want to become one
               yourself, BeBlended provides you with both options fast and easy
@@ -69,6 +71,7 @@ export default function Work() {
         <div className="workWrap">
           <div className="project">
             <p className="projectName">Coloring Cultures | May 2022 </p>
+            <p className="projectLang">React/CSS</p>
             <p className="projectDetails">
               A simple website for a mobile games company that provides more
               information about the company as well as its terms and conditions.
@@ -86,9 +89,11 @@ export default function Work() {
             <img src={Pic2} alt="Project Screenshot" />
           </div>
         </div>
+        
         <div className="workWrap">
           <div className="project">
             <p className="projectName">Cloudnimi | March 2022 </p>
+            <p className="projectLang">React/CSS</p>
             <p className="projectDetails">
               A company's website that helps their clients build businesses by
               designing & implementing production-grade business needs.
@@ -110,6 +115,7 @@ export default function Work() {
         <div className="workWrap">
           <div className="project">
             <p className="projectName">Journals Your Way | Feb 2022 </p>
+            <p className="projectLang">WordPress/Elementor</p>
             <p className="projectDetails">
               An eCommerce store that focuses on selling journals and note books
               to people across the US.
@@ -131,6 +137,7 @@ export default function Work() {
         <div className="workWrap">
           <div className="project">
             <p className="projectName">Freedom Action Now | 2021 </p>
+            <p className="projectLang">CMS Made Simple</p>
             <p className="projectDetails">
               Voter Education. Leadership Development. Community Organizing.
               Policy Advocacy.
@@ -159,6 +166,7 @@ export default function Work() {
         <div className="workWrap">
           <div className="project">
             <p className="projectName">GitHub API Project | January 2023 </p>
+            <p className="projectLang">React/CSS</p>
             <p className="projectDetails">
               This is a personal project that consumes the GitHub API and returns the list of repositories in my GitHub portfolio as well as my GitHub profile. The project is built with React and uses ContextAPI so that we are able to reuse the reponses from the API globally.
             </p>
@@ -169,7 +177,7 @@ export default function Work() {
               rel="noreferrer"
               aria-disabled
             >
-              View
+              View Site
             </a>
           </div>
           <div className="projectImage">
@@ -180,6 +188,7 @@ export default function Work() {
         <div className="workWrap">
           <div className="project">
             <p className="projectName">React Calculator | October 2022 </p>
+            <p className="projectLang">React/CSS</p>
             <p className="projectDetails">
               This is a simple calculator built with React. It is a personal project that I built to help me go over certain concepts that I had learnt.
             </p>
@@ -190,7 +199,7 @@ export default function Work() {
               rel="noreferrer"
               aria-disabled
             >
-              View
+              View Site
             </a>
           </div>
           <div className="projectImage">
@@ -201,6 +210,7 @@ export default function Work() {
         <div className="workWrap">
           <div className="project">
             <p className="projectName">Github User Search | March 2022 </p>
+            <p className="projectLang">React/Styled-components</p>
             <p className="projectDetails">
               A simple project that consumes a page of the github's Api and
               returns a list of users when a name is searched.
