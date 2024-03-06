33af0922de2c8994c8c5925660c53d94c67baf70
Lilian Okeke <lilibest30.lb@gmail.com>
2024-03-06 10:51:23 +0100

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { RiArrowRightUpLine } from "react-icons/ri";
+import { FaLinkedin, FaGithub, FaMedium } from "react-icons/fa";
+import { MdOutlineStorefront } from "react-icons/md";
 import "./style.scss";
 
 export default function Footer() {
   return (
     <footer>
       
-      <ul className="footerLinks">
+      <ul className="footerLinks mobile">
+        <li className="footerLink">
+          <a href="https://linkedin.com/in/Lilianada" target="_blank" className="socialLink" rel="noreferrer">
+            
+            <FaLinkedin size={18} style={{marginLeft: ".5rem"}} />
+          </a>
+        </li>
+        <li className="footerLink">
+          <a href="http://github.com/lilianada" className="socialLink" rel="noreferrer" target="_blank">
+            <FaGithub size={18} style={{marginLeft: ".5rem"}} />
+          </a>
+        </li>
+        <li className="footerLink">
+          <a href="https://medium.com/@theconfusedcreative" className="socialLink" rel="noreferrer" target="_blank">
+            
+            <FaMedium size={18} style={{marginLeft: ".5rem"}} />
+          </a>
+        </li>
+        <li className="footerLink">
+          <a href="https://defitcreative-aliexpress.vercel.app/" className="socialLink" rel="noreferrer" target="_blank">
+            
+            <MdOutlineStorefront size={18} style={{marginLeft: ".5rem"}} />
+          </a>
+        </li>
+      </ul>
+
+      <ul className="footerLinks desktop">
         <li className="footerLink">
           <a href="https://linkedin.com/in/Lilianada" target="_blank" className="socialLink" rel="noreferrer">
             LinkedIn
@@ -22,6 +50,12 @@ export default function Footer() {
         <li className="footerLink">
           <a href="https://medium.com/@theconfusedcreative" className="socialLink" rel="noreferrer" target="_blank">
             Medium
+            <RiArrowRightUpLine size={18} style={{marginLeft: ".5rem"}} />
+          </a>
+        </li>
+        <li className="footerLink">
+          <a href="https://defitcreative-aliexpress.vercel.app/" className="socialLink" rel="noreferrer" target="_blank">
+            Storefront
             <RiArrowRightUpLine size={18} style={{marginLeft: ".5rem"}} />
           </a>
         </li>
