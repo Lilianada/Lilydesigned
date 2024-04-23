e154695c584f9e2df2ad4c8780e10127247f4194
Lilian Okeke <lilibest30.lb@gmail.com>
2024-04-23 19:15:44 +0000

    update to home and projects page

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -5,208 +5,206 @@ import Pic1 from "../../assets/images/Journalsyourway.png";
 import Pic2 from "../../assets/images/coloringcultures.png";
 import Pic3 from "../../assets/images/C1oudnimi.png";
 import Pic4 from "../../assets/images/BeBlended.png";
-import Pic7 from "../../assets/images/Fatbossgambling.png";
+import Pic7 from "../../assets/images/BuildingBloques.png";
 import Pic10 from "../../assets/images/Thevideogamevault.png";
 import Pic11 from "../../assets/images/Funconnect.png";
 import "./style.scss";
 
 export default function Projects() {
-    return (
-        <section className="projectSection">
-            <div className="head">
-                <h3 className="title">Projects</h3>
-                <p className="text">
-                    I am constantly exploring new technologies and creative designs to
-                    develop projects that build on and go beyond my existing knowledge.
-                </p>
+  return (
+    <section className="projectSection">
+      <div className="head">
+        <h3 className="title">Projects</h3>
+        <p className="text">
+          I am constantly exploring new technologies and creative designs to
+          develop projects that build on and go beyond my existing knowledge.
+        </p>
+      </div>
+      <div className="projectList">
+        <div className="projectContainer">
+          <div className="contentContainer">
+            <img
+              className="projectImage"
+              src={Pic4}
+              alt="Screenshot of the website of BeBlended"
+            />
+            <h3 className="contentTitle">
+              BeBlended
+              <a className="arrowContainer" href="https://www.beblended.ca/">
+                {" "}
+                <BsArrowUpRight size={18} fill="#F2CBF6" />{" "}
+              </a>
+            </h3>
+            <div className="contentText">
+              <span className="lang">REACT/SCSS</span>
+              <p className="text">
+                BeBlended is a web platform that connects Black clients with
+                hairstylists specializing in their haircare needs. As a
+                developer involved in this project, I focused on building core
+                components that ensure a smooth user experience for both
+                clients, stylists and the admin.
+              </p>
             </div>
-            <div className="projectList">
-                <div className="projectContainer">
-                    <div className="contentContainer">
-                        <img
-                            className="projectImage"
-                            src={Pic10}
-                            alt="Screenshot of the website of The Video Game Vault"
-                        />
-                        <h3 className="contentTitle">
-                            VideoGameVault
-                            <a
-                                className="arrowContainer"
-                                href="https://the-videogame-vault.netlify.app/"
-                            >
-                                {" "}
-                                <BsArrowUpRight size={18} fill="#acf6f0" />{" "}
-                            </a>
-                        </h3>
-                        <div className="contentText">
-                            <span className="lang">HTML/SCSS</span>
-                            <p className="text">
-                                The Video Game Vault is a simple website for a gamer where he
-                                has all his youtube videos linked as well as articles about the
-                                games he plays.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+          </div>
+        </div>
 
-                <div className="projectContainer">
-                    <div className="contentContainer">
-                        <img
-                            className="projectImage"
-                            src={Pic11}
-                            alt="Screenshot of the website of Funconnect"
-                        />
-                        <h3 className="contentTitle">
-                            Funconnect
-                            <a className="arrowContainer" href="https://funconnect.app/">
-                                {" "}
-                                <BsArrowUpRight size={18} fill="#FFE285" />{" "}
-                            </a>
-                        </h3>
-                        <div className="contentText">
-                            <span className="lang">REACT/SCSS</span>
-                            <p className="text">
-                                Funconnect is a modern way of exploring fun places, a social way
-                                to connect like-minds, and a space to share experiences.Discover
-                                new activities and places to visit. Unlock a world of
-                                possibilities with the app.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="projectContainer">
-                    <div className="contentContainer">
-                        <img
-                            className="projectImage"
-                            src={Pic7}
-                            alt="Screenshot of the website of FatBossGambling"
-                        />
-                        <h3 className="contentTitle">
-                            FatBossGambling
-                            <a className="arrowContainer" href="https://fatbosskgasyno.pl/">
-                                {" "}
-                                <BsArrowUpRight size={18} fill="#f7c0cf" />{" "}
-                            </a>
-                        </h3>
-                        <div className="contentText">
-                            <span className="lang">HTML/CSS/AMP</span>
-                            <p className="text">
-                                FatBossGambling is an online Casino. State-of-the-art platform
-                                that offers a wide selection of exciting casino games, all
-                                powered by top-notch software. They've got all your favourites,
-                                from classic slots and table games to the latest releases.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="projectContainer">
-                    <div className="contentContainer">
-                        <img
-                            className="projectImage"
-                            src={Pic4}
-                            alt="Screenshot of the website of BeBlended"
-                        />
-                        <h3 className="contentTitle">
-                            BeBlended
-                            <a
-                                className="arrowContainer"
-                                href="https://github.com/lilianada/BeBlended"
-                            >
-                                {" "}
-                                <BsArrowUpRight size={18} fill="#F2CBF6" />{" "}
-                            </a>
-                        </h3>
-                        <div className="contentText">
-                            <span className="lang">REACT/SCSS</span>
-                            <p className="text">
-                                Whether you’re looking for a new hairstylist or want to become one yourself, BeBlended provides you with both options fast and easy within Canada.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="projectContainer">
-                    <div className="contentContainer">
-                        <img
-                            className="projectImage"
-                            src={Pic2}
-                            alt="Screenshot of the website of Coloring Cultures"
-                        />
-                        <h3 className="contentTitle">
-                            Coloring Cultures
-                            <div className="arrowContainer">
-                                {" "}
-                                <BsArrowUpRight size={18} fill="#f7d8c0" />{" "}
-                            </div>
-                        </h3>
-                        <div className="contentText">
-                            <a href="https://coloringcultures.app" className="lang">
-                                REACT/SCSS
-                            </a>
-                            <p className="text">
-                                A simple website for a mobile game company that provides more information about the company as well as its terms and conditions.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="projectContainer">
-                    <div className="contentContainer">
-                        <img
-                            className="projectImage"
-                            src={Pic3}
-                            alt="Screenshot of the website of Cloudnimi"
-                        />
-                        <h3 className="contentTitle">
-                            Cloudnimi
-                            <a
-                                className="arrowContainer"
-                                href="https://cloudnimi.netlify.app/"
-                            >
-                                {" "}
-                                <BsArrowUpRight size={18} fill="#c0cff7" />{" "}
-                            </a>
-                        </h3>
-                        <div className="contentText">
-                            <span className="lang">REACT/CSS</span>
-                            <p className="text">
-                                A company's website that helps their clients build businesses by
-                                designing & implementing production-grade business needs.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="projectContainer">
-                    <div className="contentContainer">
-                        <img
-                            className="projectImage"
-                            src={Pic1}
-                            alt="Screenshot of the website of Cloudnimi"
-                        />
-                        <h3 className="contentTitle">
-                            Journals Your Way
-                            <a
-                                className="arrowContainer"
-                                href="https://journalsyourway.com"
-                            >
-                                {" "}
-                                <BsArrowUpRight size={18} fill="#acf6f0" />{" "}
-                            </a>
-                        </h3>
-                        <div className="contentText">
-                            <span className="lang">WordPress/Elementor</span>
-                            <p className="text">
-                                An eCommerce store that focuses on selling journals and note books to people across the US.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        <div className="projectContainer">
+          <div className="contentContainer">
+            <img
+              className="projectImage"
+              src={Pic11}
+              alt="Screenshot of the website of Funconnect"
+            />
+            <h3 className="contentTitle">
+              Funconnect
+              <a className="arrowContainer" href="https://funconnect.app/">
+                {" "}
+                <BsArrowUpRight size={18} fill="#FFE285" />{" "}
+              </a>
+            </h3>
+            <div className="contentText">
+              <span className="lang">REACT/SCSS</span>
+              <p className="text">
+                Funconnect is a social platform that connects like-minds and facilitates connections between people who share similar interests. I worked closely with the ui/ux designer to create a platform that is both user friendly and visually appealing. I also implemented the backend logic that ensures the platform functions as intended.
+              </p>
             </div>
+          </div>
+        </div>
 
-            
-        </section>
-    );
+        <div className="projectContainer">
+          <div className="contentContainer">
+            <img
+              className="projectImage"
+              src={Pic3}
+              alt="Screenshot of the website of Cloudnimi"
+            />
+            <h3 className="contentTitle">
+              Cloudnimi
+              <a
+                className="arrowContainer"
+                href="https://cloudnimi.netlify.app/"
+              >
+                {" "}
+                <BsArrowUpRight size={18} fill="#c0cff7" />{" "}
+              </a>
+            </h3>
+            <div className="contentText">
+              <span className="lang">REACT/CSS</span>
+              <p className="text">
+                A company's website that helps their clients build businesses by
+                designing & implementing production-grade business needs.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="projectContainer">
+          <div className="contentContainer">
+            <img
+              className="projectImage"
+              src={Pic2}
+              alt="Screenshot of the website of Coloring Cultures"
+            />
+            <h3 className="contentTitle">
+              Coloring Cultures
+              <div className="arrowContainer">
+                {" "}
+                <BsArrowUpRight size={18} fill="#f7d8c0" />{" "}
+              </div>
+            </h3>
+            <div className="contentText">
+              <a href="https://coloringcultures.app" className="lang">
+                REACT/SCSS
+              </a>
+              <p className="text">
+                A simple website for a mobile game company that provides more
+                information about the company as well as its terms and
+                conditions.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="projectContainer">
+          <div className="contentContainer">
+            <img
+              className="projectImage"
+              src={Pic7}
+              alt="Screenshot of the website of BuilingBloques"
+            />
+            <h3 className="contentTitle">
+              Building Bloques Creative Studio
+              <a className="arrowContainer" href="https://buildingbloques.com/">
+                {" "}
+                <BsArrowUpRight size={18} fill="#f7c0cf" />{" "}
+              </a>
+            </h3>
+            <div className="contentText">
+              <span className="lang">React/Tailwind</span>
+              <p className="text">
+                BuildingBloques is a B2B platform that helps businesses bring
+                their creative visions to life. They offer a collaborative
+                approach, working closely with clients to understand their goals
+                and develop customized solutions. Their services may include
+                branding, web design, custom software development, or a
+                combination of these, depending on your specific project needs.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="projectContainer">
+          <div className="contentContainer">
+            <img
+              className="projectImage"
+              src={Pic10}
+              alt="Screenshot of the website of The Video Game Vault"
+            />
+            <h3 className="contentTitle">
+              VideoGameVault
+              <a
+                className="arrowContainer"
+                href="https://the-videogame-vault.netlify.app/"
+              >
+                {" "}
+                <BsArrowUpRight size={18} fill="#acf6f0" />{" "}
+              </a>
+            </h3>
+            <div className="contentText">
+              <span className="lang">HTML/SCSS</span>
+              <p className="text">
+                The Video Game Vault is a simple website for a gamer where he
+                has all his youtube videos linked as well as articles about the
+                games he plays.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="projectContainer">
+          <div className="contentContainer">
+            <img
+              className="projectImage"
+              src={Pic1}
+              alt="Screenshot of the website of Cloudnimi"
+            />
+            <h3 className="contentTitle">
+              Journals Your Way
+              <a className="arrowContainer" href="https://journalsyourway.com">
+                {" "}
+                <BsArrowUpRight size={18} fill="#acf6f0" />{" "}
+              </a>
+            </h3>
+            <div className="contentText">
+              <span className="lang">WordPress/Elementor</span>
+              <p className="text">
+                An eCommerce store that focuses on selling journals and note
+                books to people across the US.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import React from "react";
 import { Footer, Header } from "../components";
 import Pic from "../assets/images/LilyAvatar.jpg";
-import Resume from "../assets/images/LilianResume.pdf";
+import Resume from "../assets/images/LilianOkeke.pdf";
 import { Helmet } from "react-helmet-async";
 import "../components/Hero/style.scss";
 import { BsDownload } from "react-icons/bs";
@@ -30,7 +30,7 @@ export default function Home() {
           <h3 className="title">
             Oh Hey there <span className="wave">👋</span>
             <br />
-            I'm Lilian, a <span className="colorText"> Frontend developer</span> for high impact organizations.
+            I'm Lilian, a <span className="colorText"> Software developer</span> for high impact organizations.
           </h3>
         </div>
         <div className="aboutContent">
@@ -42,17 +42,20 @@ export default function Home() {
               <p className="text">
                 Pronouns: She/her
                 <br />
-                As a dedicated Frontend Developer specializing in both Web and Mobile Development, my primary goal is to seamlessly blend form and function in order to create immersive user experiences that align with business goals.
+                As a dedicated Software Developer specializing in frontend web development, my primary goal is to create user experiences that align with individuals, organizations, and business goals.
               </p>
               <p className="text">
-                I am proficient in utilizing cutting-edge technologies such as React.js, Vue.js for web development, and Flutter/dart for mobile applications. In addition to my core skills, my proficiency extends to HTML5, CSS3, JavaScript, TypeScript, and other associated tools.
+              I am proficient in utilizing cutting-edge technologies such as React.js, Vue.js for web development. In addition to my core skills, my proficiency extends to HTML5, CSS3, JavaScript, TypeScript, and other associated tools.
               </p>
               <p className="text">
-                With over two years of hands-on experience, I have developed and shipped innovative products to clients worldwide, ranging from startups to established corporations. 
+              With over four years of hands-on experience, I have developed and shipped innovative products and tools to clients, startups, and established corporation worldwide. I am furthering my expertise as the Lead Frontend Engineer with Supreme Marketing Limited, where I continue to enhance and refine my user interface design and development skills. I am also volunteering at She Code Africa as the Technical Lead of the frontend community where I manage the community activities, implement strategies like the Pair Learning Program which has over 200 participants and also mentor junior developers.
                 </p>
               <p className="text">
-                I maintain a fervent passion for frontend development, consistently seeking out opportunities to leverage modern frameworks and libraries to deliver aesthetically pleasing, performant, and user-centric applications. I am always eager to learn and adapt to new technologies and best practices, ensuring that the digital products I create are not only functional, but also intuitive, accessible, and visually captivating.
-              </p>
+              I am consistently seeking out opportunities to leverage modern frameworks and libraries to deliver aesthetically pleasing, performant, and user-centric applications. I am always eager to learn and adapt to new technologies and best practices, ensuring that the digital products I create are functional, intuitive, accessible, and visually captivating.
+               </p>
+               <p className="text">
+                Looking forward to connecting with professionals and organizations passionate about transforming user experiences through state-of-the-art web solutions.
+               </p>
               <a href={Resume} download className="btnPrimary">
                 <BsDownload className="downloadBtn" /> Curriculum Vitae
               </a>
@@ -69,11 +72,11 @@ export default function Home() {
                 <div className="column">
                   <div className="labels">
                     <div className="labelTitle">Engineering</div>
-                    <div className="labelValue">React.js, Vue.js, Flutter
+                    <div className="labelValue">React.js, Next.js, Typescript
                     </div>
                     <div className="labelValue">
                     </div>
-                    <div className="labelValue">HTML, CSS, SCSS, Styled-components
+                    <div className="labelValue">HTML, CSS, SCSS, Tailwind
                     </div>
                     <div className="labelValue">Firebase, Context API, Redux
                     </div>
@@ -91,7 +94,7 @@ export default function Home() {
                       Agile Software Development
                     </div>
                     <div className="labelValue">
-                      Project Management
+                      Project Management, Technical Lead
                     </div>
                   </div>
                 </div>
@@ -117,7 +120,7 @@ export default function Home() {
                       Others
                     </div>
                     <div className="labelValue">
-                      <a href="http://instagram.com/defitcreative">@Defitcreative</a>
+                      <a href="http://instagram.com/defitcreative">@defitcreative</a>
                     </div>
                   </div>
                 </div>
